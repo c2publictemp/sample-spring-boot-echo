f4fa353344120c64c724cbf5faaab27c08ccbc9e
percy6615 <percy6615_@hotmail.com>
2017-04-19 17:34:11 +0800

    update

--- src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -16,32 +16,152 @@
 
 package com.example.bot.spring.echo;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.google.common.io.ByteStreams;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.MessageContentResponse;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.action.MessageAction;
+import com.linecorp.bot.model.action.PostbackAction;
+import com.linecorp.bot.model.action.URIAction;
+import com.linecorp.bot.model.event.BeaconEvent;
 import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.FollowEvent;
+import com.linecorp.bot.model.event.JoinEvent;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.PostbackEvent;
+import com.linecorp.bot.model.event.UnfollowEvent;
+import com.linecorp.bot.model.event.message.AudioMessageContent;
+import com.linecorp.bot.model.event.message.ImageMessageContent;
+import com.linecorp.bot.model.event.message.LocationMessageContent;
+import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.message.VideoMessageContent;
+import com.linecorp.bot.model.event.source.GroupSource;
+import com.linecorp.bot.model.event.source.RoomSource;
+import com.linecorp.bot.model.event.source.Source;
+import com.linecorp.bot.model.message.AudioMessage;
+import com.linecorp.bot.model.message.ImageMessage;
+import com.linecorp.bot.model.message.ImagemapMessage;
+import com.linecorp.bot.model.message.LocationMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.StickerMessage;
+import com.linecorp.bot.model.message.TemplateMessage;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.VideoMessage;
+import com.linecorp.bot.model.message.imagemap.ImagemapArea;
+import com.linecorp.bot.model.message.imagemap.ImagemapBaseSize;
+import com.linecorp.bot.model.message.imagemap.MessageImagemapAction;
+import com.linecorp.bot.model.message.imagemap.URIImagemapAction;
+import com.linecorp.bot.model.message.template.ButtonsTemplate;
+import com.linecorp.bot.model.message.template.CarouselColumn;
+import com.linecorp.bot.model.message.template.CarouselTemplate;
+import com.linecorp.bot.model.message.template.ConfirmTemplate;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import lombok.NonNull;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EchoApplication.class, args);
-    }
+	@Autowired
+    private LineMessagingClient lineMessagingClient;
+	
+	static Path downloadedContentDir;
+	public static void main(String[] args) {
+		SpringApplication.run(EchoApplication.class, args);
+	}
 
-    @EventMapping
-    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-//        System.out.println("event: " + event);
-    	System.out.println(event.getSource().getUserId());
-        return new TextMessage(event.getMessage().getText());
-    }
+	@EventMapping
+	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		// System.out.println("event: " + event);
+		System.out.println(event.getSource().getUserId());
 
-    @EventMapping
-    public void handleDefaultMessageEvent(Event event) {
-//        System.out.println("event: " + event);
-    }
+		// can use
+		// TextMessage textMessage = new TextMessage("hello");
+		// PushMessage pushMessage = new PushMessage(
+		// "Uc7a46420c8125d4fcaa0312f2d47dc10",
+		// textMessage
+		// );
+		//
+		// Response<BotApiResponse> response = null;
+		// try {
+		// response = LineMessagingServiceBuilder
+		// .create("pslrmKmSM30059ArgBD2wv4Sm6zRbyjqpdrYIHFZbtGZmqO76wuOBV5p2+re039F7umgZptlue+RUiv+k38Oin6v1DIt5wfS8myZ1Xw3h7RPRDczDJgakudp0I8EheQ+VLE77SiMvDtMGUxcg7nvXAdB04t89/1O/w1cDnyilFU=")
+		// .build()
+		// .pushMessage(pushMessage)
+		// .execute();
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// System.out.println(response.code() + " " + response.message());
+		System.out.println(event.getMessage().getText());
+		return new TextMessage("²Â³J¶Ç§¾ªü");
+		// return new TextMessage(event.getMessage().getText());
+
+	}
+
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		// System.out.println("event: " + event);
+	}
+//	 @Bean
+//	  public EmbeddedServletContainerFactory servletContainer() {
+//	    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+//	        @Override
+//	        protected void postProcessContext(Context context) {
+//	          SecurityConstraint securityConstraint = new SecurityConstraint();
+//	          securityConstraint.setUserConstraint("CONFIDENTIAL");
+//	          SecurityCollection collection = new SecurityCollection();
+//	          collection.addPattern("/*");
+//	          securityConstraint.addCollection(collection);
+//	          context.addConstraint(securityConstraint);
+//	        }
+//	      };
+//	    
+//	    tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+//	    return tomcat;
+//	  }
+//	  
+//	  private Connector initiateHttpConnector() {
+//	    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//	    connector.setScheme("https");
+//	    connector.setPort(8080);
+//	    connector.setSecure(false);
+//	    connector.setRedirectPort(8443);
+//	    
+//	    return connector;
+//	  }
 }
